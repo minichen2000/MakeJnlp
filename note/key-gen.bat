3f82a793abcb3dc93a7abb7ab9ecfd5d214b4258
minichen <minichen@qq.com>
2016-09-28 14:51:09 +0800

    ..

--- note/key-gen.bat
+++ note/key-gen.bat
@@ -1,1 +1,3 @@
-keytool -genkey -keystore myKeys -alias jdc
+keytool -genkey -keystore nsbENMSKeys -alias enmskey -validity 3650
+rem keytool -export -keystore nsbENMSKeys -alias enmskey -file nsbENMSKeys.cert
+rem keytool –certreq –keystore nsbENMSKeys -alias enmskey
